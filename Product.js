ec8e0ba5edff512bf4713cdd2ebe2c0b1704e451
ajayjamage3 <ajayjamage3@gmail.com>
2022-12-15 13:13:54 +0530

    added navbar and foeeter to product page

--- Product.js
+++ Product.js
@@ -35,3 +35,8 @@ function renderData(data){
   
 }
 
+import { dropmenu, navbar} from "./exoportNavBar.js";
+import { footer } from "./footer.js";
+document.querySelector("nav").innerHTML = navbar()
+document.getElementById("drop").innerHTML = dropmenu()
+document.getElementById("bottom-footer").innerHTML = footer()
